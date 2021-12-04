f26dd914f4dfbeab37be659735a0db96cdd3fee6
Iurii-B <53317383+Iurii-B@users.noreply.github.com>
2021-12-04 22:20:11 +0300

    Create kubernetes.tf

---
+++ Terraform/kubernetes.tf
@@ -0,0 +1,102 @@
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+# To configure kubectl run "aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terraform output -raw cluster_name)"
+
+
+resource "kubernetes_namespace" "ns" {
+  metadata {
+    name = var.k8s_namespace
+  }
+}
+
+
+resource "kubernetes_deployment" "deploy" {
+
+  lifecycle {
+    ignore_changes = [spec,]
+  }
+
+  metadata {
+    name = "flaskprod"
+    namespace = var.k8s_namespace
+    labels = {
+      app = "flaskprod"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "flaskprod"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "flaskprod"
+        }
+      }
+
+      spec {
+        container {
+          image = var.image_init
+          name  = "flaskprod"
+        
+          resources {
+            limits = {
+              cpu    = "250m"
+              memory = "100Mi"
+            }
+            requests = {
+              cpu    = "50m"
+              memory = "50Mi"
+            }
+          }
+
+          env {
+              name  = "DB_ADMIN_USERNAME"
+              value = var.db_username
+          }
+          env {
+              name  = "DB_ADMIN_PASSWORD"
+              value = var.db_password
+          }
+          env {
+              name  = "DB_URL"
+              value = aws_db_instance.database1.address
+          }
+        }  
+      }
+    }
+  }
+}
+
+# To install Metrics Server run "kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml"
+
+resource "kubernetes_service" "elb" {
+
+  metadata {
+    name = "tf-elb"
+    namespace = var.k8s_namespace
+  }
+
+  spec {
+    selector = {
+      app = "flaskprod"
+    }
+
+    port {
+      port        = 5000
+      target_port = 5000
+    }
+
+    type = "LoadBalancer"
+
+  }
+}
